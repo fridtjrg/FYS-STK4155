381d1ac964566c7c2a32ba4ad289d3c931d79a14
fridtjrg <fridtjof@gjengset.com>
2021-09-28 17:07:16 +0200

    adding useful code folder

--- projects/1project/task1.py
+++ projects/1project/task1.py
@@ -10,6 +10,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 import statistics
+from time import time
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 
 
@@ -139,9 +142,44 @@ print(MSE(z_test,zpredict))
 
 
 
+#------Task 2------
+
+
+# Returns mean of bootstrap samples 
+# Bootstrap algorithm
+def bootstrap(data, datapoints):
+    t = np.zeros(datapoints)
+    n = len(data)
+    # non-parametric bootstrap         
+    for i in range(datapoints):
+        t[i] = np.mean(data[np.random.randint(0,n,n)])
+    # analysis    
+    print("Bootstrap Statistics :")
+    print("original           bias      std. error")
+    print("%8g %8g %14g %15g" % (np.mean(data), np.std(data),np.mean(t),np.std(t)))
+    return t
+
+
+datapoints = 1000
+
+t = bootstrap(X_train,datapoints)
+
+
+    
+n, binsboot, patches = plt.hist(t, 50, density=True, facecolor='red', alpha=0.75)
+# add a 'best fit' line  
+y = norm.pdf(binsboot, np.mean(t), np.std(t))
+lt = plt.plot(binsboot, y, 'b', linewidth=1)
+plt.xlabel('x')
+plt.ylabel('Probability')
+plt.grid(True)
+plt.show()
+
+
 """
 What to plot? (use mesh, x,y, z and z_tilda?)
-How to find confidence? y-y_tilda
+How to find confidence? y-y_tilda = sigma
+Sima is the stardard deviation of the error?
 
 print("Beta(ols) variance:") //variance of beta? or = np.mean( np.var(y_pred, axis=1, keepdims=True) )
 print(statistics.variance(ols_beta))
