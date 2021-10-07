5e2bf5dd0eeabbbe72eafdda029948ecff6a7a74
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-10-07 09:24:30 +0200

    Start merging branches

--- projects/1project/task3.py
+++ projects/1project/task3.py
@@ -1,20 +1,25 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import numpy as np
+# The MIT License (MIT)
+#
+# Copyright © 2021 Fridtjof Gjengset, Adele Zaini, Gaute Holen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 from random import random, seed
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-import statistics
-from time import time
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-from ols_solver import FrankeFunction, create_X
+from linear_regression import FrankeFunction, create_X
 from crossvalidation import cross_validation
 
 
@@ -52,6 +57,5 @@ plt.xlabel("complexity")
 plt.ylabel("MSE")
 plt.title("Plot of the MSE as a function of complexity of the model")
 plt.legend()
-plt.grid()     
-#plt.savefig('Task2plot(n='+str(n)+').pdf')
+plt.grid()
 plt.show() 
