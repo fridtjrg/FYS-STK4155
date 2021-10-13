32142f2f25bf0c2906acae09a94fe2066b0945ef
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-10-13 12:28:09 +0200

    Swap names task45a _b

--- projects/1project/task45a.py
+++ projects/1project/task45a.py
@@ -14,33 +14,61 @@
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from numpy.core.function_base import logspace
-from regan import *
 import numpy as np
+from random import random, seed
+from regan import create_xyz_dataset, create_X, Split_and_Scale, OLS_solver, MSE, R2, ridge_reg, lasso_reg
+import matplotlib.pyplot as plt
 
-savefigures = True
+savefigure = True
 
 np.random.seed(1234)
 
+#Degree of polynomial
+degree=5
 # Datapoints (squared root of datapoints -> meshgrid)
-n = 30
+n = 25
 # Paramaters of noise distribution
 mu_N = 0; sigma_N = 0.2
-# Max degree of complexity
-maxdegree = 20
 
 # Create vanilla dataset:
 x,y,z = create_xyz_dataset(n,mu_N, sigma_N)
 
-lambdas = [10**x for x in [-12, -6, -3, 0, 3]]
+X = create_X(x, y, degree)
+X_train, X_test, z_train, z_test = Split_and_Scale(X,np.ravel(z)) #StardardScaler, test_size=0.2, scale=true
 
-run_plot_compare(z, 100, N=n, k=5,poly_degree = 18,plot=True, saveplots=savefigures, foldername = 'Task5')
+ols_beta, ols_model, ols_predict  = OLS_solver(X_train, X_test, z_train, z_test)
+print("Training MSE for OLS:", round(MSE(z_train,ols_model),4))
+print("Test MSE for OLS", round(MSE(z_test,ols_predict),4))
 
-compare_lmd_BS(z, n, lambdas, maxdegree, solver = 'RIDGE', n_resampling = 100, saveplots = savefigures, foldername = 'Task4')
-compare_lmd_CV(z, n, 5, lambdas, maxdegree, solver = 'RIDGE', saveplots = savefigures, foldername = 'Task4')
-compare_lmd_CV(z, n, 10, lambdas, maxdegree, solver = 'RIDGE', saveplots = savefigures, foldername = 'Task4')
+lambdas = np.logspace(-30,10,num=50)
 
-compare_lmd_BS(z, n, lambdas, maxdegree, solver = 'LASSO', n_resampling = 100, saveplots = savefigures, foldername = 'Task5')
-compare_lmd_CV(z, n, 5, lambdas, maxdegree, solver = 'LASSO', saveplots = savefigures, foldername = 'Task5')
-compare_lmd_CV(z, n, 10, lambdas, maxdegree, solver = 'LASSO', saveplots = savefigures, foldername = 'Task5')
+MSE_lmd_ridge_train = []
+MSE_lmd_ridge_test = []
+MSE_lmd_lasso_train = []
+MSE_lmd_lasso_test = []
 
+for lmd in lambdas:
+    _,ridge_model ,ridge_predict  = ridge_reg(X_train, X_test, z_train, z_test, lmd=lmd)
+    lasso_model,lasso_predict  = lasso_reg(X_train, X_test, z_train, z_test, lmd=lmd)
+    MSE_lmd_ridge_train.append(MSE(ridge_model,z_train))
+    MSE_lmd_ridge_test.append(MSE(ridge_predict,z_test))
+    MSE_lmd_lasso_train.append(MSE(lasso_model,z_train))
+    MSE_lmd_lasso_test.append(MSE(lasso_predict,z_test))
+
+
+fig = plt.figure(figsize = ( 10, 7))
+ax = fig.add_subplot(1,1,1)
+ax.plot(lambdas,MSE_lmd_ridge_train, label ="Ridge train", color = 'red', linestyle = 'dashed')  
+ax.plot(lambdas,MSE_lmd_ridge_test, label ="Ridge test", color = 'red')
+ax.plot(lambdas,MSE_lmd_lasso_train, label ="Lasso train", color = 'green', linestyle = 'dashed')  
+ax.plot(lambdas,MSE_lmd_lasso_test, label ="Lasso test", color = 'green')  
+ax.set_xscale('log')
+
+plt.xlabel("$\lambda$")
+plt.ylabel("MSE")
+plt.title(f"Plot of the MSE for different $\lambda$ for complexity {degree}")
+plt.legend()
+plt.grid()
+if savefigure:
+    plt.savefig("../reports/Proj1_Plots/Task5/MSE_lambdas.png")
+plt.show() 
