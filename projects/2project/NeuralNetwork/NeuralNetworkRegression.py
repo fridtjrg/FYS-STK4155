de1c701b36172b7b0dd1f42c0f96859af523fa33
paulgiraudIMT <paul.giraud@imt-atlantique.net>
2021-11-09 20:14:47 +0100

    regression

---
+++ projects/2project/NeuralNetwork/NeuralNetworkRegression.py
@@ -0,0 +1,163 @@
+import numpy as np
+
+class DenseLayer:
+
+    def __init__(self, n_hidden_neurons, n_categories,type_activation,lmbda, eta):
+
+        self.weights = np.random.randn(n_hidden_neurons, n_categories)
+        self.bias = np.zeros(n_categories) + 0.01
+        self.n_hidden_neurons = n_hidden_neurons
+        self.n_categories = n_categories
+        self.type_activation = type_activation
+        self.lmbda = lmbda
+        self.eta = eta
+
+
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def tanh(self, x):
+        return np.tanh(x)
+
+    def relu(self, x):
+        row, col = x.shape
+        for i in range(row):
+            for j in range(col):
+                x[i][j] = max(0, x[i][j])
+        return x
+
+    def reluPrime(self, x):
+        row, col = x.shape
+        for i in range(row):
+            for j in range(col):
+                if x[i][j] > 0:
+                    x[i][j] = 1
+                else:
+                    x[i][j] = 0
+        return x
+
+    def tanhPrime(self, x):
+
+        return 1 - x ** 2
+
+    def sigmoidPrime(self, x):
+        return x * ( 1 - x)
+
+    def activation(self, x):
+        if self.type_activation is None:
+            return x
+
+        elif (self.type_activation == 'sigmoid'):
+            return self.sigmoid(x)
+
+        elif (self.type_activation == 'tanh'):
+            return self.tanh(x)
+
+        elif (self.type_activation == 'relu'):
+            return self.relu(x)
+
+    def activationPrime(self, x):
+
+        if self.type_activation is None:
+            return x
+
+        elif (self.type_activation == 'sigmoid'):
+            return self.sigmoidPrime(x)
+
+        elif (self.type_activation == 'tanh'):
+            return self.tanhPrime(x)
+
+        elif (self.type_activation == 'relu'):
+            return self.reluPrime(x)
+
+    def feedForward(self, x):
+
+        self.last_activation = self.activation(np.dot(x, self.weights) + self.bias)
+        return self.last_activation
+
+    def backPropagation(self,err,layer_prev,  X = None):
+
+        error = np.dot(err , layer_prev.weights.T) * self.activationPrime(self.last_activation)
+        if X is None :
+            self.weights_grad = np.dot(self.last_activation.T, error)
+        else:
+            self.weights_grad = np.dot(X.T, error)
+
+        self.bias_grad = np.sum(error, axis = 0)
+
+        if self.lmbda > 0.0:
+
+            self.weights_grad += self.lmbda * self.weights
+
+        self.weights -= self.eta * self.weights_grad
+        self.bias -= self.eta * self.bias_grad
+
+        return error
+
+
+class NeuralNetwork:
+
+    def __init__(self, X, y, n_epochs, batch_size):
+        self.epochs = n_epochs
+        self.X = X
+        self.y = y
+        self.iterations = X.shape[0] // batch_size
+        self._layers = []
+        self.batch_size = batch_size
+        self.loss = []
+
+    def add_layer(self, layer):
+        self._layers.append(layer)
+
+    def feed_forward(self):
+        X = self.X_batch
+        for layer in self._layers:
+            X = layer.feedForward(X)
+        self.pred = X
+
+
+    def backpropagation(self):
+        err =self.pred - np.atleast_2d(self.y_batch).T
+        output_layer = self._layers[-1]
+        output_layer.weights_grad = np.dot(self._layers[len(self._layers)-2].last_activation.T, err)
+        output_layer.bias_grad = np.sum(err, axis = 0)
+        if output_layer.lmbda > 0.0:
+
+            output_layer.weights_grad += output_layer.lmbda * output_layer.weights
+
+        output_layer.weights -= output_layer.eta * output_layer.weights_grad
+        output_layer.bias -= output_layer.eta * output_layer.bias_grad
+
+        for i in reversed(range(1, len(self._layers)-1)):
+            layer = self._layers[i]
+            layer_prev = self._layers[i+1]
+            err = layer.backPropagation(err, layer_prev)
+        layer = self._layers[0]
+        err = layer.backPropagation(err, self._layers[1],self.X_batch)
+
+
+
+    def train(self):
+        data_indices = np.arange(self.X.shape[0])
+        for i in range(self.epochs):
+            for j in range(self.iterations):
+                # pick datapoints with replacement
+                chosen_datapoints = np.random.choice(
+                    data_indices, size=self.batch_size, replace=False
+                )
+                # minibatch training data
+                self.X_batch = self.X[chosen_datapoints]
+                self.y_batch = self.y[chosen_datapoints]
+                self.feed_forward()
+                self.backpropagation()
+            mse = np.mean((self.y - self.predict(self.X))**2)
+            self.loss.append(mse)
+            print("epoche :",i+1, "MSE : ", mse)
+
+    def predict(self, X):
+        for layer in self._layers:
+            X = layer.feedForward(X)
+        return X
+
+    def getLoss(self):
+        return self.loss
--- projects/2project/NeuralNetwork/SolutionNNRegression.py
+++ projects/2project/NeuralNetwork/SolutionNNRegression.py
@@ -1,58 +1,68 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+import tensorflow as tf
 import numpy as np
-
-#======================= NeuralNetwork
-
-from NeuralNetwork import NeuralNetwork
-from Layer import Layer
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+from NeuralNetworkRegression import DenseLayer, NeuralNetwork
 
 #====================== DATA
 import sys
 sys.path.append("../Data")
-from DataRegression import X, X_test, X_train, x, x_mesh, y_mesh, z_test, z_train, plotFunction, x_y_test, x_y_train, x_y
+from DataRegression import X, X_test, X_train, x, x_mesh, y_mesh, z_test, z_train, plotFunction, x_y_test, x_y_train, x_y, z
 
-
+N = len(x)
 
 # Hessian Matrix
-H = (2.0/len(x))* X_train.T @ X_train
+H = (2.0/N)* X_train.T @ X_train
 # Get the eigenvalues
 EigValues, EigVectors = np.linalg.eig(H)
 eta_max = 1.0/np.max(EigValues)
 
+n_hidden_neurons = 50
+batch_size = 5
+epochs = 100
 
-def solution_nn():
+activation_function = 'sigmoid'
+optimizer = tf.keras.optimizers.Adam(learning_rate= eta_max)
 
-    MSE_nn_val = []
 
-    #eta = np.logspace(-5, -3, 1)
-    eta = [eta_max]
-    for i in eta:
-        nn = NeuralNetwork()
-        nn.add_layer(Layer(x_y_train.shape[1], 32, 'sigmoid'))
-        nn.add_layer(Layer(32, 32, 'sigmoid'))
-        nn.add_layer(Layer(32, 32, 'sigmoid'))
-        nn.add_layer(Layer(32, 1, None))
-        train = nn.train(x_y_train, z_train, i, nb_epochs = 100, batch_size = 5,  lmbd=0.9, _type='regression')
-        y_pred = nn.predict(x_y_test, 'regression')
-        z_pred = nn.predict(x_y, 'regression')
-        MSE_nn_val.append(nn.MSE((y_pred.flatten()), z_test))
-        plotFunction(x_mesh, y_mesh, z_pred.reshape(len(x), len(x)), "prediction")
+model = Sequential()
+model.add(Dense(n_hidden_neurons, activation = activation_function, input_dim = X_train.shape[1]))
+model.add(Dense(units = n_hidden_neurons, activation = activation_function))
+model.add(Dense(units = 1))
+model.compile(optimizer = optimizer, loss = 'mean_squared_error')
+model.fit(X_train, z_train, batch_size = batch_size, epochs = epochs)
 
-    print(MSE_nn_val)
-    plot, ax = plt.subplots()
-    plt.title('MSE for the OLS, Ridge and Neural Networks (validation data)')
-    plt.semilogx(eta, MSE_nn_val, 'b-o', label='Neural Networks')
-    plt.xlabel(r'Learning rate $\eta$')
-    plt.ylabel('MSE')
-    #ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    plt.legend()
-    plt.subplots_adjust(left=0.2, bottom=0.2, right=0.9)
 
-    return plt.show()
+nn = NeuralNetwork(x_y_train, z_train, n_epochs = epochs, batch_size = batch_size)
+nn.add_layer(DenseLayer(x_y_train.shape[1], n_hidden_neurons, 'sigmoid', lmbda=0.001, eta=eta_max))
+nn.add_layer(DenseLayer(n_hidden_neurons, n_hidden_neurons, 'sigmoid', lmbda=0.001, eta=eta_max))
+nn.add_layer(DenseLayer(n_hidden_neurons, 1, None, lmbda= 0.001, eta=eta_max))
+nn.train()
 
 
 
-solution_nn()
+loss_values = model.history.history['loss']
 
+z_plot = model.predict(X)
+ytilde = nn.predict(x_y)
+
+
+plotFunction(x_mesh, y_mesh, z.reshape(len(x), len(x)), "data")
+plotFunction(x_mesh, y_mesh, ytilde.reshape(len(x), len(x)), "prediction")
+plotFunction(x_mesh, y_mesh, z_plot.reshape(len(x), len(x)), "Prediction")
+
+
+MSE_nn_val = nn.getLoss()
+plot, ax = plt.subplots()
+plt.title('Training MSE ')
+plt.plot(range(1, epochs+1), MSE_nn_val, 'b-o', label='Training MSE own NN')
+plt.plot(range(1, epochs+1), loss_values, 'r-o', label='Training MSE keras')
+plt.title('Training loss keras')
+plt.xlabel('Epochs')
+plt.ylabel('MSE')
+plt.legend()
+
+
+
+plt.show()
