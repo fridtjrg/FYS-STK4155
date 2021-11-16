efc40b8f51575e734dcebfb87b5b21ceecd4c278
paulgiraudIMT <paul.giraud@imt-atlantique.net>
2021-11-16 17:26:49 +0100

    MSE keras NN

--- projects/2project/NeuralNetwork/NeuralNetworkRegression.py
+++ projects/2project/NeuralNetwork/NeuralNetworkRegression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class DenseLayer:
-
+    # important paramaters of a layer
     def __init__(self, n_hidden_neurons, n_categories,type_activation,lmbda, eta):
 
         self.weights = np.random.randn(n_hidden_neurons, n_categories)
@@ -12,6 +12,7 @@ class DenseLayer:
         self.lmbda = lmbda
         self.eta = eta
 
+    # ========================= Activation function
 
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
@@ -25,6 +26,9 @@ class DenseLayer:
             for j in range(col):
                 x[i][j] = max(0, x[i][j])
         return x
+
+    #======================= Differential activation function
+
 
     def reluPrime(self, x):
         row, col = x.shape
@@ -70,8 +74,11 @@ class DenseLayer:
         elif (self.type_activation == 'relu'):
             return self.reluPrime(x)
 
-    def feedForward(self, x):
+    #================== Feed forward
 
+
+    def feedForward(self, x):
+        #return the value of the activation function of a vector mutply by the weight of the layer plus the bias of the layer
         self.last_activation = self.activation(np.dot(x, self.weights) + self.bias)
         return self.last_activation
 
@@ -95,8 +102,15 @@ class DenseLayer:
         return error
 
 
+
+
+
+
+
 class NeuralNetwork:
 
+
+    # important paramaters of a NN
     def __init__(self, X, y, n_epochs, batch_size):
         self.epochs = n_epochs
         self.X = X
@@ -109,7 +123,10 @@ class NeuralNetwork:
     def add_layer(self, layer):
         self._layers.append(layer)
 
+    #================== Feed forward
+
     def feed_forward(self):
+        #we pass our data to all the activation functions of our layers
         X = self.X_batch
         for layer in self._layers:
             X = layer.feedForward(X)
@@ -136,7 +153,7 @@ class NeuralNetwork:
         err = layer.backPropagation(err, self._layers[1],self.X_batch)
 
 
-
+    #train the NN
     def train(self):
         data_indices = np.arange(self.X.shape[0])
         for i in range(self.epochs):
@@ -148,12 +165,15 @@ class NeuralNetwork:
                 # minibatch training data
                 self.X_batch = self.X[chosen_datapoints]
                 self.y_batch = self.y[chosen_datapoints]
+                #feed forward
                 self.feed_forward()
+                #back propagation
                 self.backpropagation()
             mse = np.mean((self.y - self.predict(self.X))**2)
             self.loss.append(mse)
             #print("epoch :",i+1, "MSE : ", mse)
 
+    #return our prediction for a set of data
     def predict(self, X):
         for layer in self._layers:
             X = layer.feedForward(X)
--- projects/2project/SGD/SDG.py
+++ projects/2project/SGD/SDG.py
@@ -9,6 +9,7 @@ import numpy as np
 
 class SDG:
 
+    #important parameters of our SGD
     def __init__(self, learning_rate,n_epochs,batch_size, method = 'ols', lmbda = 0.01):
         self.n_epochs = n_epochs
         self.batch_size = batch_size
@@ -17,23 +18,23 @@ class SDG:
         self.lmbda = lmbda
 
 
-
+    #return the MSE
     def compute_square_loss(self, X, y, theta):
         m = len(y)
         loss = (1.0 / m) * (np.linalg.norm((X.dot(theta) - y)) ** 2)
         return loss
 
-
+    #return the ridge gradient
     def gradient_ridge(self, X, y, beta, lambda_):
         return 2 * (np.dot(X.T, (X.dot(beta) - y))) + 2 * lambda_ * beta
 
-
+    #return the OLS gradient
     def gradient_ols(self, X, y, beta):
         m = X.shape[0]
         grad = 2 / m * X.T.dot(X.dot(beta) - y)
         return grad
 
-
+    #return a set of mini-batchs
     def iterate_minibatches(self, inputs, targets, batchsize):
         assert inputs.shape[0] == targets.shape[0]
         indices = np.random.permutation(inputs.shape[0])
@@ -42,29 +43,31 @@ class SDG:
             excerpt = indices[start_idx:end_idx]
             yield inputs[excerpt], targets[excerpt]
 
+    #return MSE for Ols and Ridge
     def compute_test_mse(self, X_test, y_test, beta, lambda_=0.01):
         mse_ols_test = self.compute_square_loss(X_test, y_test, beta)
         mse_ridge_test = self.compute_square_loss(X_test, y_test, beta) + lambda_ * np.dot(beta.T, beta)
         return mse_ols_test, mse_ridge_test
 
-
+    #SGD
     def train(self, X, y):
         num_instances, num_features = X.shape[0], X.shape[1]
         beta = np.random.randn(num_features)
 
+        #for each epochs
         for epoch in range(self.n_epochs + 1):
-
+            #For each batchs
             for batch in self.iterate_minibatches(X, y, self.batch_size):
 
                 X_batch, y_batch = batch
-
+                #if we use OLS we compute with gradient OLS (either RIDGE)
                 if self.method == 'ols':
                     gradient = self.gradient_ols(X_batch, y_batch, beta)
                     beta = beta - self.learning_rate * gradient
                 if self.method == 'ridge':
                     gradient = self.gradient_ridge(X_batch, y_batch, beta, lambda_= self.lmbda)
                     beta = beta - self.learning_rate * gradient
-
+        #compute MSE
         mse_ols_train = self.compute_square_loss(X, y, beta)
         mse_ridge_train = self.compute_square_loss(X, y, beta) + self.lmbda * np.dot(beta.T, beta)
 
